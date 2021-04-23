f58e33ae352e5914d29e8653cab57f3bebdfe7b3
HoangNam-Phan <hoangnamphan@outlook.de>
2021-04-23 16:14:05 +0200

    dropdown if no list

--- src/components/AddToList.js
+++ src/components/AddToList.js
@@ -4,7 +4,7 @@ import Dropdown from 'react-bootstrap/Dropdown'
 const AddToList = ({ name, rating, id, addToList }) => {
 
     const [lists, setLists] = useState();
- 
+
     // API-CALL: FETCH ALLER LISTEN
     useEffect(() => {
         fetch('https://myfavoriteshows.herokuapp.com/lists')
@@ -17,14 +17,17 @@ const AddToList = ({ name, rating, id, addToList }) => {
 
     return (
         <>
-        {/* ERSTELLT DROPDOWN BUTTON FÜR DAS HINZUFÜGEN EINER SHOW IN EINE LISTE */}
+            {/* ERSTELLT DROPDOWN BUTTON FÜR DAS HINZUFÜGEN EINER SHOW IN EINE LISTE */}
             {
-                lists &&
-                lists.map((list) => (
-                    <Dropdown.Item onClick={() => addToList(name, rating.average, id, list._id)} key={list._id}>
-                        {list.name}
+                lists ?
+                    lists.map((list) => (
+                        <Dropdown.Item onClick={() => addToList(name, rating.average, id, list._id)} key={list._id}>
+                            {list.name}
+                        </Dropdown.Item>
+                    ))
+                    : <Dropdown.Item>
+                        No lists yet!
                     </Dropdown.Item>
-                ))
             }
         </>
     )
